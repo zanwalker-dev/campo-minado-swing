ee1e70b819f0d57512e7497c533bc2094ecfa481
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-31 12:26:03 -0300

    update: win conditions tested

--- src/com/zanwalkerdev/campominado/view/Temp.java
+++ src/com/zanwalkerdev/campominado/view/Temp.java
@@ -7,6 +7,14 @@ public class Temp {
     public static void main(String[] args) {
 
         Board board = new Board(3,3, 9);
-        board.open(2,2);
+        board.toggleMark(0,0);
+        board.toggleMark(0,1);
+        board.toggleMark(0,2);
+        board.toggleMark(1,0);
+        board.toggleMark(1,1);
+        board.toggleMark(1,2);
+        board.toggleMark(2,0);
+        board.toggleMark(2,1);
+        board.toggleMark(2,2);
     }
 }

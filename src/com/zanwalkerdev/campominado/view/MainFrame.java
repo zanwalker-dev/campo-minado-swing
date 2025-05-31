5dc5a8d62f088b4e269730aadcefae56a56392d7
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-31 12:47:15 -0300

    feat: instanced Board

--- src/com/zanwalkerdev/campominado/view/MainFrame.java
+++ src/com/zanwalkerdev/campominado/view/MainFrame.java
@@ -1,10 +1,15 @@
 package com.zanwalkerdev.campominado.view;
 
+import com.zanwalkerdev.campominado.model.Board;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
 
     public MainFrame(){
+
+        Board board = new Board(16,30, 50);
+
         setTitle("Campo Minado");
         setSize(690, 438);
         setLocationRelativeTo(null);

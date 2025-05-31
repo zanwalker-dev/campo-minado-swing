cad8b5adfdf0a99e9e7ede21f37cdc3ae1eafc26
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-31 12:23:18 -0300

    update: testing

--- src/com/zanwalkerdev/campominado/view/Temp.java
+++ src/com/zanwalkerdev/campominado/view/Temp.java
@@ -1,8 +1,12 @@
 package com.zanwalkerdev.campominado.view;
 
+import com.zanwalkerdev.campominado.model.Board;
+
 public class Temp {
 
     public static void main(String[] args) {
 
+        Board board = new Board(3,3, 9);
+        board.open(2,2);
     }
 }

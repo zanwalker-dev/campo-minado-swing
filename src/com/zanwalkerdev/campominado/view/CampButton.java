3efc8710a379115edf0b0275ba0e8c2513e275c7
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-02 10:02:09 -0300

    feat: camp button initialized

---
+++ src/com/zanwalkerdev/campominado/view/CampButton.java
@@ -0,0 +1,12 @@
+package com.zanwalkerdev.campominado.view;
+
+import com.zanwalkerdev.campominado.model.Camp;
+
+import javax.swing.*;
+
+public class CampButton extends JButton {
+
+    public CampButton(Camp camp) {
+
+    }
+}

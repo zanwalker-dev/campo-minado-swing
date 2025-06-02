870d73a1f238d40135ef4978131e982f61bde46c
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-02 10:54:07 -0300

    update: switch implemented with Enum classes

--- src/com/zanwalkerdev/campominado/view/CampButton.java
+++ src/com/zanwalkerdev/campominado/view/CampButton.java
@@ -1,12 +1,44 @@
 package com.zanwalkerdev.campominado.view;
 
 import com.zanwalkerdev.campominado.model.Camp;
+import com.zanwalkerdev.campominado.model.CampEvent;
+import com.zanwalkerdev.campominado.model.ObserverCamp;
 
 import javax.swing.*;
 
-public class CampButton extends JButton {
+public class CampButton extends JButton  implements ObserverCamp {
 
     public CampButton(Camp camp) {
 
     }
+
+    @Override
+    public void eventHappened(Camp camp, CampEvent event) {
+        switch (event) {
+            case OPEN:
+                applyStyleOpen();
+                break;
+            case MARK:
+                applyStyleMark();
+                break;
+            case EXPLODE:
+                applyStyleExplode();
+                break;
+            default:
+                applyStyleDefault();
+        }
+    }
+
+    private void applyStyleOpen() {
+    }
+
+    private void applyStyleMark() {
+    }
+
+    private void applyStyleExplode() {
+    }
+
+    private void applyStyleDefault() {
+    }
+
 }

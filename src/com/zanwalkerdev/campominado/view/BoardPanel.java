f962cc8759fc5d4b4e24b052b22d679561f5aa2a
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-02 10:04:20 -0300

    update: camp buttons now is generated through foreach

--- src/com/zanwalkerdev/campominado/view/BoardPanel.java
+++ src/com/zanwalkerdev/campominado/view/BoardPanel.java
@@ -3,10 +3,16 @@ package com.zanwalkerdev.campominado.view;
 import com.zanwalkerdev.campominado.model.Board;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BoardPanel extends JPanel {
 
     public BoardPanel(Board board){
-        
+
+        setLayout(new GridLayout(board.getLines(), board.getColumns()));
+
+        int total = board.getLines() * board.getColumns();
+
+        board.forEach(camp -> add(new CampButton(camp)));
     }
 }
